dce6d498924ba76c06dd40cce79d8adae3668e6a
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-02-03 13:22:14 +0100

    Set up make, clangformat

--- gendata.c
+++ gendata.c
@@ -1,1 +1,11 @@
-int main(void){}
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+uint8_t buf[256];
+
+int main(void) {
+  while (fgets((char *)buf, sizeof(buf), stdin))
+    ;
+}
